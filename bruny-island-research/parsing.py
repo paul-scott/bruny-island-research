a479dac48bc35e4a94e248846f65767fb41ff79c
Paul Scott <paul.m.sco@gmail.com>
2019-04-10 08:57:19 +1000

    Enable extracting forecasts in chunks

--- bruny-island-research/parsing.py
+++ bruny-island-research/parsing.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import datetime as dt
 import psycopg2 as sql
+import json
 
 
 def resource_to_bigint(resource):
@@ -74,13 +75,55 @@ def select_forecasts(conn, rid, fr, to):
             return [extract_forecasts_data(row) for row in curs.fetchall()]
 
 
+def process(conn, rid, fr, to):
+    rid_int = resource_to_bigint(rid)
+    actuals = select_estimated_actuals(conn, rid_int, fr, to)
+    logging.info('Actuals contains {} rows'.format(len(actuals)))
+    actuals_fn = 'est_actuals_{}_{}_{}.csv'.format(
+        rid,
+        fr.strftime('%Y-%m-%dT%H:%M:%S%z'),
+        to.strftime('%Y-%m-%dT%H:%M:%S%z'),
+    )
+    with open(actuals_fn, 'w') as f:
+        for row in actuals:
+            r = row[0].strftime('%Y-%m-%dT%H:%M:%S%z')
+            r += ',' + str(row[1])
+            r += '\n'
+            f.write(r)
+
+    forecasts_fn = 'forecasts_{}_{}_{}.csv'.format(
+        rid,
+        fr.strftime('%Y-%m-%dT%H:%M:%S%z'),
+        to.strftime('%Y-%m-%dT%H:%M:%S%z'),
+    )
+    with open(forecasts_fn, 'w') as f:
+        # Reading forecasts is slow, cutting into chunks
+        fr_chunk = fr
+        to_chunk = fr_chunk + dt.timedelta(days=2)
+        while True:
+            if fr_chunk >= to:
+                break
+            logging.info('Chunk {}'.format(fr_chunk))
+            to_chunk = min(to, to_chunk)
+            forecasts = select_forecasts(conn, rid_int, fr_chunk, to_chunk)
+            logging.info('Forecasts contains {} rows'.format(len(forecasts)))
+            for row in forecasts:
+                r = row[0].strftime('%Y-%m-%dT%H:%M:%S%z')
+                r += ','
+                r += ','.join([str(v) for v in row[1:]])
+                r += '\n'
+                f.write(r)
+            fr_chunk = to_chunk
+            to_chunk = fr_chunk + dt.timedelta(days=2)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('database', help='database name')
     parser.add_argument('rid',
-                        help='resource id')
+                        help='resource id file or value')
     parser.add_argument('--fr',
                         help='iso8601 datetime from (default: now - 24hrs)')
     parser.add_argument('--to',
@@ -100,30 +143,18 @@ if __name__ == '__main__':
         if args.to is None:
             to = fr + dt.timedelta(hours=24)
 
-    conn = sql.connect(dbname=args.database)
-    #conn = sql.connect(dbname=args.database, host='localhost', port=5433)
+    #conn = sql.connect(dbname=args.database)
+    conn = sql.connect(dbname=args.database, host='localhost', port=5433)
 
-    rid = resource_to_bigint(args.rid)
+    rids = []
+    try:
+        rfile = json.load(open(args.rid))
+        for r in rfile['sites']:
+            rids.append(r['resource_id'])
 
-    actuals = select_estimated_actuals(conn, rid, fr, to)
-    logging.info('Actuals contains {} rows'.format(len(actuals)))
-    actuals_fn = ('est_actuals_' + fr.strftime('%Y-%m-%dT%H:%M:%S%z')
-                  + '_' + to.strftime('%Y-%m-%dT%H:%M:%S%z') + '.csv')
-    with open(actuals_fn, 'w') as f:
-        for row in actuals:
-            r = row[0].strftime('%Y-%m-%dT%H:%M:%S%z')
-            r += ',' + str(row[1])
-            r += '\n'
-            f.write(r)
+    except:
+        rids = [args.rid]
 
-    forecasts = select_forecasts(conn, rid, fr, to)
-    logging.info('Forecasts contains {} rows'.format(len(forecasts)))
-    forecasts_fn = ('forecasts_' + fr.strftime('%Y-%m-%dT%H:%M:%S%z')
-                    + '_' + to.strftime('%Y-%m-%dT%H:%M:%S%z') + '.csv')
-    with open(forecasts_fn, 'w') as f:
-        for row in forecasts:
-            r = row[0].strftime('%Y-%m-%dT%H:%M:%S%z')
-            r += ','
-            r += ','.join([str(v) for v in row[1:]])
-            r += '\n'
-            f.write(r)
+    print(rids)
+    for rid in rids:
+        process(conn, rid, fr, to)
